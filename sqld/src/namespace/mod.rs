9414b5a4a2b841641b9408394d593b5239ef7425
ad hoc <postma.marin@protonmail.com>
2023-09-01 15:02:47 +0200

    fix rebase quirks

--- sqld/src/namespace/mod.rs
+++ sqld/src/namespace/mod.rs
@@ -177,7 +177,7 @@ impl MakeNamespace for ReplicaNamespaceMaker {
         _from: &Namespace<Self::Database>,
         _to: Bytes,
     ) -> crate::Result<Namespace<Self::Database>> {
-        return Err(ForkError::ForkReplica.into())
+        return Err(ForkError::ForkReplica.into());
     }
 }
 
