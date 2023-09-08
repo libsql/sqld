95668cc49632085c02d80d746f0ccc2db7c74eaa
ad hoc <postma.marin@protonmail.com>
2023-09-08 17:53:45 +0200

    add turmoil types for testing

--- sqld/src/net.rs
+++ sqld/src/net.rs
@@ -32,7 +32,9 @@ where
 }
 
 pub trait Conn:
-    AsyncRead + AsyncWrite + Unpin + Send + 'static + Connected<ConnectInfo = TcpConnectInfo> { }
+    AsyncRead + AsyncWrite + Unpin + Send + 'static + Connected<ConnectInfo = TcpConnectInfo>
+{
+}
 
 pub trait Accept: HyperAccept<Conn = Self::Connection, Error = IoError> + Send + 'static {
     type Connection: Conn;
@@ -79,9 +81,9 @@ impl HyperAccept for AddrIncoming {
 }
 
 pin_project! {
-    pub struct AddrStream {
+    pub struct AddrStream<S = tokio::net::TcpStream> {
         #[pin]
-        stream: tokio::net::TcpStream,
+        stream: S,
         remote_addr: SocketAddr,
         local_addr: SocketAddr,
     }
@@ -91,9 +93,12 @@ impl Accept for AddrIncoming {
     type Connection = AddrStream;
 }
 
-impl Conn for AddrStream { }
+impl<S> Conn for AddrStream<S> where S: Unpin + AsyncWrite + AsyncRead + Send + 'static {}
 
-impl AsyncRead for AddrStream {
+impl<S> AsyncRead for AddrStream<S>
+where
+    S: AsyncRead + AsyncWrite,
+{
     fn poll_read(
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
@@ -103,7 +108,10 @@ impl AsyncRead for AddrStream {
     }
 }
 
-impl AsyncWrite for AddrStream {
+impl<S> AsyncWrite for AddrStream<S>
+where
+    S: AsyncRead + AsyncWrite,
+{
     fn poll_write(
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
@@ -127,10 +135,84 @@ impl AsyncWrite for AddrStream {
     }
 }
 
-impl Connected for AddrStream {
+impl<S> Connected for AddrStream<S> {
     type ConnectInfo = TcpConnectInfo;
 
     fn connect_info(&self) -> Self::ConnectInfo {
-        self.stream.connect_info()
+        TcpConnectInfo {
+            local_addr: Some(self.local_addr),
+            remote_addr: Some(self.remote_addr),
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use futures_core::Future;
+    use tower::Service;
+
+    use super::*;
+
+    type TurmoilAddrStream = AddrStream<turmoil::net::TcpStream>;
+
+    pub struct TurmoilAcceptor {
+        acceptor: Pin<
+            Box<dyn HyperAccept<Conn = TurmoilAddrStream, Error = IoError> + Send + Sync + 'static>,
+        >,
+    }
+
+    impl TurmoilAcceptor {
+        pub async fn bind(addr: SocketAddr) -> std::io::Result<Self> {
+            let stream = async_stream::stream! {
+                let listener = turmoil::net::TcpListener::bind(addr).await?;
+                loop {
+                    yield listener.accept().await.and_then(|(stream, remote_addr)| Ok(AddrStream {
+                        remote_addr,
+                        local_addr: stream.local_addr()?,
+                        stream,
+                    }));
+                }
+            };
+            let acceptor = hyper::server::accept::from_stream(stream);
+            Ok(Self {
+                acceptor: Box::pin(acceptor),
+            })
+        }
+    }
+
+    impl Accept for TurmoilAcceptor {
+        type Connection = TurmoilAddrStream;
+    }
+
+    impl HyperAccept for TurmoilAcceptor {
+        type Conn = TurmoilAddrStream;
+        type Error = IoError;
+
+        fn poll_accept(
+            mut self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+        ) -> Poll<Option<Result<Self::Conn, Self::Error>>> {
+            self.acceptor.as_mut().poll_accept(cx)
+        }
+    }
+
+    struct TurmoilConnector;
+
+    impl Service<Uri> for TurmoilConnector {
+        type Response = turmoil::net::TcpStream;
+        type Error = IoError;
+        type Future = Pin<Box<dyn Future<Output = std::io::Result<Self::Response>>>>;
+
+        fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn call(&mut self, uri: Uri) -> Self::Future {
+            Box::pin(async move {
+                let addr = turmoil::lookup(uri.host().unwrap());
+                let port = uri.port().unwrap().as_u16();
+                turmoil::net::TcpStream::connect((addr, port)).await
+            })
+        }
     }
 }
