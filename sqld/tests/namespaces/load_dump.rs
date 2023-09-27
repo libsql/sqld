2ef424cb5f72c6ab628fdf3ab882cee3945a3f3c
ad hoc <postma.marin@protonmail.com>
2023-09-27 15:38:37 +0200

    add create dump test.

--- sqld/tests/namespaces/load_dump.rs
+++ sqld/tests/namespaces/load_dump.rs
@@ -1,6 +1,7 @@
 use std::convert::Infallible;
 
 use hyper::{service::make_service_fn, Response, Body, StatusCode};
+use insta::assert_snapshot;
 use libsql::{Database, Value};
 use serde_json::json;
 use tempfile::tempdir;
@@ -157,14 +158,46 @@ fn load_namespace_from_no_txn() {
 
     sim.client("client", async move {
         let client = Client::new();
-        let resp = client.post("http://primary:9090/v1/namespaces/foo/create", json!({ "dump_url": format!("file:{}", tmp_path.join("dump.sql").display())})).await.unwrap();
+        let resp = client.post("http://primary:9090/v1/namespaces/foo/create", json!({ "dump_url": format!("file:{}", tmp_path.join("dump.sql").display())})).await?;
         // the dump is malformed
         assert_eq!(resp.status(), StatusCode::BAD_REQUEST, "{}", resp.json::<serde_json::Value>().await.unwrap_or_default());
+        assert_snapshot!(resp.json_value().await.unwrap());
 
         // namespace doesn't exist
         let foo = Database::open_remote_with_connector("http://foo.primary:8080", "", TurmoilConnector)?;
         let foo_conn = foo.connect()?;
         assert!(foo_conn.query("select count(*) from test", ()).await.is_err());
+
+        Ok(())
+    });
+
+    sim.run().unwrap();
+}
+
+#[test]
+fn export_dump() {
+    let mut sim = Builder::new().build();
+    let tmp = tempdir().unwrap();
+
+    make_primary(&mut sim, tmp.path().to_path_buf());
+
+    sim.client("client", async move {
+        let client = Client::new();
+        let resp = client.post("http://primary:9090/v1/namespaces/foo/create", json!({})).await?;
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_snapshot!(resp.json_value().await.unwrap());
+
+        let foo = Database::open_remote_with_connector("http://foo.primary:8080", "", TurmoilConnector)?;
+        let foo_conn = foo.connect()?;
+        foo_conn.execute("create table test (x)", ()).await?;
+        foo_conn.execute("insert into test values (42)", ()).await?;
+        foo_conn.execute("insert into test values (\"foo\")", ()).await?;
+        foo_conn.execute("insert into test values (\"bar\")", ()).await?;
+
+
+        let resp = client.get("http://foo.primary:8080/dump").await?;
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_snapshot!(resp.body_string().await?);
 
         Ok(())
     });
