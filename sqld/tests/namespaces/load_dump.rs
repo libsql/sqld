8d3b24f3c960deb259feab42bf9e83b6559110c6
ad hoc <postma.marin@protonmail.com>
2023-09-27 15:20:30 +0200

    test malformed dumps

--- sqld/tests/namespaces/load_dump.rs
+++ sqld/tests/namespaces/load_dump.rs
@@ -104,7 +104,7 @@ fn load_namespace_from_dump_from_file() {
 
 #[test]
 #[ignore = "need to remove block in place in load dump, wait for #693"]
-fn load_namespace_from_invalid_dump() {
+fn load_namespace_from_no_commit() {
     const DUMP: &str = r#"
     PRAGMA foreign_keys=OFF;
     BEGIN TRANSACTION;
@@ -123,8 +123,45 @@ fn load_namespace_from_invalid_dump() {
     sim.client("client", async move {
         let client = Client::new();
         let resp = client.post("http://primary:9090/v1/namespaces/foo/create", json!({ "dump_url": format!("file:{}", tmp_path.join("dump.sql").display())})).await.unwrap();
-        assert_eq!(resp.status(), StatusCode::OK, "{}", resp.json::<serde_json::Value>().await.unwrap_or_default());
+        // the dump is malformed
+        assert_eq!(resp.status(), StatusCode::BAD_REQUEST, "{}", resp.json::<serde_json::Value>().await.unwrap_or_default());
 
+        // namespace doesn't exist
+        let foo = Database::open_remote_with_connector("http://foo.primary:8080", "", TurmoilConnector)?;
+        let foo_conn = foo.connect()?;
+        assert!(foo_conn.query("select count(*) from test", ()).await.is_err());
+
+        Ok(())
+    });
+
+    sim.run().unwrap();
+}
+
+#[test]
+#[ignore = "need to remove block in place in load dump, wait for #693"]
+fn load_namespace_from_no_txn() {
+    const DUMP: &str = r#"
+    PRAGMA foreign_keys=OFF;
+    CREATE TABLE test (x);
+    INSERT INTO test VALUES(42);
+    COMMIT;
+    "#;
+
+    let mut sim = Builder::new().build();
+    let tmp = tempdir().unwrap();
+    let tmp_path = tmp.path().to_path_buf();
+
+    std::fs::write(tmp_path.join("dump.sql"), &DUMP).unwrap();
+
+    make_primary(&mut sim, tmp.path().to_path_buf());
+
+    sim.client("client", async move {
+        let client = Client::new();
+        let resp = client.post("http://primary:9090/v1/namespaces/foo/create", json!({ "dump_url": format!("file:{}", tmp_path.join("dump.sql").display())})).await.unwrap();
+        // the dump is malformed
+        assert_eq!(resp.status(), StatusCode::BAD_REQUEST, "{}", resp.json::<serde_json::Value>().await.unwrap_or_default());
+
+        // namespace doesn't exist
         let foo = Database::open_remote_with_connector("http://foo.primary:8080", "", TurmoilConnector)?;
         let foo_conn = foo.connect()?;
         assert!(foo_conn.query("select count(*) from test", ()).await.is_err());
